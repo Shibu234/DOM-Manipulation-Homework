220cc5927c900d33d80ccbbdb9210d8e28121b33
Shibu234 <160158981+Shibu234@users.noreply.github.com>
2024-07-30 19:58:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+let box = document.getElementById("box")
+document.body.style.display="flex"
+document.body.style.alignItems='center'
+document.body.style.justifyContent='center'
+document.body.style.height='100vh'
+document.body.style.marginLeft='75vh'
+
+box.style.textAlign="center"
+box.style.alignContent="center"
+
+box.style.height="10vw"
+box.style.width="10vw"
+box.style.padding="10vw"
+box.style.border="0.25rem solid blueviolet"
+box.style.backgroundColor="aquamarine"
+box.style.color="blueviolet"
+box.style.fontSize="2rem"
+box.style.fontWeight="1000"
